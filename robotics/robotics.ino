9ba0ec184072d3fe823c586d68517d3e910f1d1b
Estherwanjir <estherwachira1425@gmail.com>
2023-03-31 10:02:37 +0300

    robotics

---
+++ robotics/robotics.ino
@@ -0,0 +1,17 @@
+//motor 1
+int ena=36;
+int in1=34;
+int in2=35;
+//motor2
+int enb=39;
+int in3=32;
+int in4=33;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
