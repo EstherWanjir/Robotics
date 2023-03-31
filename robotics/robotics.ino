f84fdae1797b22c240b5a56177ef2738ca48f62f
Estherwanjir <estherwachira1425@gmail.com>
2023-03-31 10:57:49 +0300

    robotics

--- robotics/robotics.ino
+++ robotics/robotics.ino
@@ -1,17 +1,60 @@
 //motor 1
-int ena=36;
-int in1=34;
-int in2=35;
+int ena=25;
+int in1=26;
+int in2=27;
 //motor2
-int enb=39;
+int enb=14;
 int in3=32;
 int in4=33;
+
+void forward(){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
+
+  analogWrite(ena,150);
+  analogWrite(enb,150);
+  
+}
+void reverse(){
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+
+  digitalWrite(in3,LOW);
+  digitalWrite(in4,HIGH);
+
+  analogWrite(ena,150);
+  analogWrite(enb,150);
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
+   // put your setup code here, to run once:
+   pinMode(ena,OUTPUT);
+   pinMode(in1,OUTPUT);
+   pinMode(in2,OUTPUT);
+   pinMode(enb,OUTPUT);
+   pinMode(in3,OUTPUT);
+   pinMode(in4,OUTPUT);
+
+   ledcAttachPin(ena,2);
+   ledcAttachPin(enb,2);
+   ledcSetup(0,1000,8);
+
 
 }
 
+
 void loop() {
   // put your main code here, to run repeatedly:
+  forward();
+
+  delay(5000);
+
+  reverse();
+
+  delay(5000);
 
 }
